f65fc18faec5d5a841b71a27583bb4114bf1cb3c
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-18 20:12:25 +0800

    feat(core): support sass

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'tsup'
+import { sassPlugin as sass } from 'esbuild-sass-plugin'
 
 import html from '@plasticine-cropper/esbuild-plugin-html'
 
@@ -15,9 +16,13 @@ export default defineConfig([
   },
   {
     entry: {
-      'plasticine-cropper': 'src/styles/plasticine-cropper.css',
+      'plasticine-cropper': 'src/styles/plasticine-cropper.scss',
     },
     outDir: 'dist',
     sourcemap: true,
+    esbuildPlugins: [
+      // @ts-ignore -- tsup 依赖的 esbuild 版本为 `^0.18.2`，`esbuild-sass-plugin` 依赖的 esbuild 版本为 `^0.19.1`，Plugin 类型不兼容，但是不影响运行时，故忽略类型检查
+      sass(),
+    ],
   },
 ])
