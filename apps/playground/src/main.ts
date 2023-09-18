a4d893a27c6d3519a04194a19be316e426634753
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-18 19:55:26 +0800

    feat(playground): import style of @plasticine-cropper/core

--- apps/playground/src/main.ts
+++ apps/playground/src/main.ts
@@ -1,4 +1,5 @@
 import { CropperImpl } from '@plasticine-cropper/core'
+import '@plasticine-cropper/core/dist/plasticine-cropper.css'
 
 const imageElement = document.querySelector<HTMLImageElement>('#playground-image')
 
