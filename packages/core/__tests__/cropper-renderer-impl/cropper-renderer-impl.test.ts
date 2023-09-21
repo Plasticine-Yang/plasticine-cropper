680116a8526d1f260488448c12f9ea5a2c32b419
yangwenfeng <yangwenfeng.0719@bytedance.com>
2023-09-21 21:19:06 +0800

    test(core): 补齐 cropper renderer impl 相关测试

--- packages/core/__tests__/cropper-renderer-impl/cropper-renderer-impl.test.ts
+++ packages/core/__tests__/cropper-renderer-impl/cropper-renderer-impl.test.ts
@@ -1,7 +1,7 @@
 import { CropperRendererImpl } from '@internal-dist/index'
 
 describe('cropper renderer impl', () => {
-  test('should get all cropper elements', () => {
+  const setup = () => {
     const containerElement = document.createElement('div')
     const imageElement = document.createElement('img')
 
@@ -9,6 +9,54 @@ describe('cropper renderer impl', () => {
 
     const cropperRendererImpl = new CropperRendererImpl(imageElement)
 
-    expect(cropperRendererImpl).toBeDefined()
+    return {
+      cropperRendererImpl,
+      imageElement,
+    }
+  }
+
+  test('should throw error without container', () => {
+    const imageElement = document.createElement('img')
+
+    const internalTest = () => {
+      new CropperRendererImpl(imageElement)
+    }
+    expect(internalTest).toThrowErrorMatchingInlineSnapshot('"未发现图片的容器元素，无法渲染 plasticine cropper"')
+  })
+
+  test('should get all cropper elements', () => {
+    const { cropperRendererImpl } = setup()
+
+    expect(cropperRendererImpl.getCropperElements()).toMatchSnapshot()
+  })
+
+  test('should toggle visibility of raw image element', () => {
+    const { cropperRendererImpl, imageElement } = setup()
+
+    // hide
+    cropperRendererImpl.hideRawImageElement()
+    expect(Array.from(imageElement.classList)).toMatchInlineSnapshot(`
+      [
+        "plasticine-cropper-common-hidden",
+      ]
+    `)
+
+    // show
+    cropperRendererImpl.showRawImageElement()
+    expect(Array.from(imageElement.classList)).toMatchInlineSnapshot('[]')
+  })
+
+  test('should move crop container', () => {
+    const { cropperRendererImpl } = setup()
+    const { cropContainer } = cropperRendererImpl.getCropperElements()
+
+    cropperRendererImpl.moveCropContainer(400, 300)
+
+    expect({ top: cropContainer.style.top, left: cropContainer.style.left }).toMatchInlineSnapshot(`
+      {
+        "left": "400px",
+        "top": "300px",
+      }
+    `)
   })
 })
